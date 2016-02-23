8785e0da152fad8229a3806f1bdc0e22503858b7
jeFFF <tisseur2toile@gmail.com>
2016-02-23 13:00:37 +0100

    Listing comics

--- wp-content/themes/topten/assets/javascript/custom/custom.js
+++ wp-content/themes/topten/assets/javascript/custom/custom.js
@@ -16,7 +16,7 @@ $(function() {
 		var myMd = md5(ts+_PRV_KEY+_API_KEY);
 
 		var mySearch = $('#s').val();
-
+$('.resultats').html('');
 		//var myUrl = 'http://gateway.marvel.com/v1/public/comics?dateRange=2016-02-24%2C2016-02-24&orderBy=onsaleDate&limit=10&offset=10&ts='+ts+'&apikey='+_API_KEY+"&hash="+myMd;
 		var myUrl = 'http://gateway.marvel.com/v1/public/series?title='+mySearch+'&ts='+ts+'&apikey='+_API_KEY+"&hash="+myMd;
 
@@ -37,8 +37,8 @@ $(function() {
 				{
 					var serie = response.data.results[c];
 					var html = "";
-					html += '<img src="'+serie.thumbnail.path+'.jpg">';
-					html += '<h3>'+serie.title+'</h3>';
+					html += '<div class="small-3 columns"><a href="#'+serie.id+'"><img src="'+serie.thumbnail.path+'.jpg">';
+					html += '<h3>'+serie.title+'</h3></a></div>';
 					$('.resultats').append(html);
 					console.log(serie.title);
 				}
@@ -56,7 +56,53 @@ $(function() {
 
     }
   });
+
+
 	//});
 });
 
+});
+$(window).on('hashchange', function() {
+	var myHash = window.location.hash;
+	myHash = myHash.replace('#','');
+	var ts = new Date().getTime();
+	var myMd = md5(ts+_PRV_KEY+_API_KEY);
+	$('.resultats').html('');
+	var newUrl = 'http://gateway.marvel.com/v1/public/comics?series='+myHash+'&ts='+ts+'&apikey='+_API_KEY+"&hash="+myMd;
+	$.ajax({
+		url: newUrl ,
+		beforeSend: function( xhr ) {
+			xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+		}
+	})
+	.done(function( response ) {
+		response = JSON.parse(response);
+		if ( console && console.log ) {
+		console.log( "Sample of data:", response);//response.data.results[0] );
+
+		if (response.data.count > 0)
+		{
+			for (var c in response.data.results)
+			{
+				var serie = response.data.results[c];
+				var html = "";
+				html += '<div class="small-3 columns"><a href="#'+serie.id+'"><img src="'+serie.thumbnail.path+'.jpg">';
+				html += '<h3>'+serie.title+'</h3></a></div>';
+				$('.resultats').append(html);
+				console.log(serie.title);
+			}
+		}
+		else
+		{
+			alert('Aucun résultat');
+		}
+
+		/*var myHTML = $(".clone").html();
+		myHTML = myHTML.replace("{{DATE}}", response.data.results[0].dates[0].date);
+		myHTML = myHTML.replace("{{COVER}}", response.data.results[0].thumbnail.path+'.jpg');
+		$('body').append(myHTML);*/
+
+
+}
+});
 });
